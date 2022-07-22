5d64b2467180a745829521050221beba54417c4a
Whalebro <steven.chou.second@gmail.com>
2022-07-22 16:04:22 +0800

    Dynamic API Endpoint

--- database/controller.js
+++ database/controller.js
@@ -23,8 +23,10 @@ export async function getUser(req, res) {
 
     if (userId) {
       const user = await Users.findById(userId)
+
       res.status(200).json(user)
     }
+
     res.status(404).json({ error: 'User not Selected...!' })
   } catch (error) {
     res.status(404).json({ error: 'Cannot get the User...!' })
