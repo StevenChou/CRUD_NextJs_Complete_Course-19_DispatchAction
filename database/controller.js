ce22946455c460beb40e868eedaa2ce8f670eaba
Whalebro <steven.chou.second@gmail.com>
2022-07-22 15:09:50 +0800

    post

--- database/controller.js
+++ database/controller.js
@@ -35,8 +35,11 @@ export async function getUser(req, res) {
 export async function postUser(req, res) {
   try {
     const formData = req.body
-    if (!formData)
+
+    if (!formData) {
       return res.status(404).json({ error: 'Form Data Not Provided...!' })
+    }
+
     Users.create(formData, function (err, data) {
       return res.status(200).json(data)
     })
