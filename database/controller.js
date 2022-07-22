3e97e4541e7d84d4b7ccbbbda68982ac0a03fea5
Whalebro <steven.chou.second@gmail.com>
2022-07-22 14:49:12 +0800

    Schema & Controllers

--- database/controller.js
+++ database/controller.js
@@ -6,7 +6,10 @@ export async function getUsers(req, res) {
   try {
     const users = await Users.find({})
 
-    if (!users) return res.status(404).json({ error: 'Data not Found' })
+    if (!users) {
+      return res.status(404).json({ error: 'Data not Found' })
+    }
+
     res.status(200).json(users)
   } catch (error) {
     res.status(404).json({ error: 'Error While Fetching Data' })
