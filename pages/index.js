a3598f12d5e1f1b100c86964b7f158dd1535983b
Whalebro <steven.chou.second@gmail.com>
2022-08-04 15:31:31 +0800

    修正 pwd

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Home() {
         <div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
           <div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
             <h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
-              會員登入
+              🤑🤑🤑 VIP 會員登入 🤑🤑🤑
             </h1>
             <form
               className='space-y-4 md:space-y-6'
@@ -99,7 +99,7 @@ export default function Home() {
                     minLength: 3,
                   })}
                   placeholder='••••••••'
-                  className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                  className={!errors.password ? normalInput : errorInput}
                 />
                 {errors.password && (
                   <p className='mt-2 text-sm text-red-600 dark:text-red-500'>
@@ -113,7 +113,7 @@ export default function Home() {
                 type='submit'
                 className='w-full text-white bg-indigo-500 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
               >
-                登入
+                入坑
               </button>
             </form>
           </div>
