40839735e4770f5db5d3ec486fdd1eee4256768b
Whalebro <steven.chou.second@gmail.com>
2022-08-05 22:32:53 +0800

    add twin & styled component

--- pages/form/sample1.js
+++ pages/form/sample1.js
@@ -1,9 +1,38 @@
 import { useForm } from 'react-hook-form'
+import tw, { styled } from 'twin.macro'
 
 import Layout from '@/components/main/Layout'
 
-const normalInput =
-  'bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+const TailwindInput = tw.input`
+  bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary focus:border-primary block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
+`
+
+const TailwindCheckbox = tw.input`
+  w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600
+`
+
+const TailwindSelect = tw.select`
+  bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500
+`
+
+const TailwindRadio = tw.input`
+  w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600
+`
+
+const TailwindButton = tw.button`
+  flex h-10 mt-7 bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-gray-50 hover:border-indigo-500 hover:text-gray-800
+`
+
+const ConditionalButton = styled.button(({ isRed }) => [
+  isRed
+    ? tw`bg-red-300 hover:bg-red-700`
+    : tw`bg-indigo-500 hover:border-indigo-700`,
+  tw`
+    flex h-10 mt-7
+    text-white px-4 py-2 border rounded-md
+    hover:text-gray-800
+  `,
+])
 
 export default function Sample1() {
   const { register, handleSubmit, setValue } = useForm()
@@ -25,11 +54,7 @@ export default function Sample1() {
           >
             First Name
           </label>
-          <input
-            placeholder='bill'
-            {...register('firstName')}
-            className={normalInput}
-          />
+          <TailwindInput placeholder='bill' {...register('firstName')} />
         </div>
 
         <div>
@@ -39,11 +64,7 @@ export default function Sample1() {
           >
             Last Name
           </label>
-          <input
-            placeholder='luo'
-            {...register('lastName')}
-            className={normalInput}
-          />
+          <TailwindInput placeholder='luo' {...register('lastName')} />
         </div>
 
         <div>
@@ -53,21 +74,19 @@ export default function Sample1() {
           >
             Email
           </label>
-          <input
+          <TailwindInput
             placeholder='bluebill1049@hotmail.com'
             type='email'
             {...register('email')}
-            className={normalInput}
           />
         </div>
 
         <div>
           <div className='flex items-center mb-4'>
-            <input
+            <TailwindCheckbox
               id='default-checkbox'
               type='checkbox'
               {...register('isDeveloper')}
-              className='w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
             />
             <label
               htmlFor='default-checkbox'
@@ -82,13 +101,10 @@ export default function Sample1() {
           <label className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'>
             Age group
           </label>
-          <select
-            {...register('ageGroup')}
-            className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-          >
+          <TailwindSelect {...register('ageGroup')}>
             <option value='0'>0 - 1</option>
             <option value='1'>1 - 100</option>
-          </select>
+          </TailwindSelect>
         </div>
 
         <div>
@@ -97,12 +113,11 @@ export default function Sample1() {
           </label>
           <div className='flex'>
             <div className='flex items-center mr-4'>
-              <input
+              <TailwindRadio
                 id='inline-radio'
                 type='radio'
                 {...register('sex')}
                 value='male'
-                className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
               />
               <label
                 htmlFor='inline-radio'
@@ -112,12 +127,11 @@ export default function Sample1() {
               </label>
             </div>
             <div className='flex items-center mr-4'>
-              <input
+              <TailwindRadio
                 id='inline-2-radio'
                 type='radio'
                 {...register('sex')}
                 value='female'
-                className='w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600'
               />
               <label
                 htmlFor='inline-2-radio'
@@ -129,8 +143,7 @@ export default function Sample1() {
           </div>
         </div>
 
-        <button
-          className='flex h-10 mt-7 bg-indigo-500 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800'
+        <TailwindButton
           type='button'
           onClick={() => {
             setValue('firstName', 'Set value by action')
@@ -140,10 +153,8 @@ export default function Sample1() {
           }}
         >
           Set All Values
-        </button>
-        <button className='flex h-10 mt-7 bg-rose-300 text-white px-4 py-2 border rounded-md hover:bg-grary-50 hover:border-indigo-500 hover:text-gray-800'>
-          Submit
-        </button>
+        </TailwindButton>
+        <ConditionalButton isRed>Submit</ConditionalButton>
       </form>
     </div>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    './public/**/*.html',
+    './node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
     extend: {
